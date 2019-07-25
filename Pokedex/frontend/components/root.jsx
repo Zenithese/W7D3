2afe8b970674fa334d218144e31834c551b7ed74
Justin <jandersen770@gmail.com>
2019-07-25 16:23:08 -0400

    phase 4 / adding the router

--- Pokedex/frontend/components/root.jsx
+++ Pokedex/frontend/components/root.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import PokemonIndexContainer from './pokemon/pokemon_index_container';
+import { HashRouter, Route } from 'react-router-dom';
 
 export const Root = ({store}) => (
-  <Provider store={store}>
-    <PokemonIndexContainer/>
-  </Provider>
+  <HashRouter>
+    <Provider store={store}>
+      <PokemonIndexContainer/>
+    </Provider>
+  </HashRouter>
 );
--- Pokedex/frontend/pokedex.jsx
+++ Pokedex/frontend/pokedex.jsx
@@ -5,6 +5,8 @@ import { receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions'
 import configureStore from './store/store';
 import {selectAllPokemon} from './reducers/selectors';
 import {Root} from './components/root';
+import { HashRouter, Route } from 'react-router-dom';
+
 document.addEventListener('DOMContentLoaded', () => {
   let store = configureStore();
 
